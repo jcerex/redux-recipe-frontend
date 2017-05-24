4afec1197bf9d71dab4a9492ac7cfaf3eeb101b7
Jamie Cerexhe <jcerexhe@gmail.com>
2017-05-24 11:04:10 +1000

    https api call

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   };
 
   getIngredientList() {
-    const URL = 'http://yamagucci.herokuapp.com/api/ingredients?key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNob3VoZWkueWFtYXVjaGlAbGl2ZS5jb20iLCJpYXQiOjE0OTQ5OTMxMTV9.G0ctQghRRAqaZiGSZyT5Oi-YXUUfb3UsYQpsmMaVA0k';
+    const URL = 'https://yamagucci.herokuapp.com/api/ingredients?key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNob3VoZWkueWFtYXVjaGlAbGl2ZS5jb20iLCJpYXQiOjE0OTQ5OTMxMTV9.G0ctQghRRAqaZiGSZyT5Oi-YXUUfb3UsYQpsmMaVA0k';
     axios.get(URL)
       .then((response) => {
         this.setState({ ingredients: response.data });
